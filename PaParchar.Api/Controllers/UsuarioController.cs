eb0826ce9e228e510c84d1b404cdafc1f0067917
Samuel Rendon <samuel.rendon@emast.co>
2025-05-14 00:03:20 -0500

    feature(usuarios): crud completo

--- PaParchar.Api/Controllers/UsuarioController.cs
+++ PaParchar.Api/Controllers/UsuarioController.cs
@@ -20,11 +20,34 @@ namespace PaParchar.Api.Controllers
             return result.ToHttpResponse();
         }
 
+        [HttpGet("{usuarioId}")]
+        public async Task<ActionResult<ShowUsuarioDto>> ShowUsuario(Guid usuarioId)
+        {
+            IResult<ShowUsuarioDto> result = await this.usuarioService.GetProjectedByIdAsync<ShowUsuarioDto>(usuarioId);
+
+            return result.ToHttpResponse();
+        }
+
         [HttpPost]
         public async Task<ActionResult<ShowUsuarioDto>> CreateUsuario([FromBody] CreateUsuarioDto usuarioDto)
         {
             IResult<ShowUsuarioDto> result = await this.usuarioService.CreateUsuario(usuarioDto);
 
+            return result.ToHttpResponse();
+        }
+
+        [HttpPut("{usuarioId}")]
+        public async Task<ActionResult<ShowUsuarioDto>> UpdateUsuario(Guid usuarioId, [FromBody] UpdateUsuarioDto usuarioDto)
+        {
+            IResult<ShowUsuarioDto> result = await this.usuarioService.UpdateFromDto<ShowUsuarioDto, UpdateUsuarioDto>(usuarioId, usuarioDto);
+
+            return result.ToHttpResponse();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<object>> DeleteParche(Guid id)
+        {
+            IResult<object> result = await this.usuarioService.DeleteAsync(id);
             return result.ToHttpResponse();
         }
     }
---
+++ PaParchar.Application/DTOs/Usuario/UpdateUsuarioDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PaParchar.Application.DTOs.Usuario
+{
+    public class UpdateUsuarioDto
+    {
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+    }
+}
